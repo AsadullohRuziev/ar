65fce3f16350259d6e7a8eccb9ad229822f9ea90
Intizom <asadullohruziev@gmail.com>
2022-03-07 20:17:39 +0500

    hahaaga

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import Link from "./Link";
-import {FaReact, FaHtml5} from 'react-icons/fa'
 
 const Content = () => {
   return <div className=" bg-orange-300 pb-96">
-                <h1 className="pb-24 text-center text-3xl">Portifolios</h1>
+                <h1 className="pb-24 text-center text-3xl">Portifolio</h1>
             <div className="w-3/4 m-auto flex justify-between flex-wrap	">
                 <Link link={"https://github.com/AsadullohRuziev/education"} title={'Education'} ></Link>
                 <Link link={"https://github.com/AsadullohRuziev/restaurant"} title={'Restaurant'}></Link>
