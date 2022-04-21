4923c67b83669bce12445eebc57e8f4c166db45f
AsadullohRuziev <asadullohruziev@gmail.com>
2022-04-21 12:37:17 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import Aboutme from './components/Aboutme'
 import Contact from './components/Contact'
+import Link from './components/Link'
 import Main from './components/Main'
 
 
 const App = () => {
   return (
     <div>
+        <Link/>
         <Contact/>
         <Aboutme/>
         <Main/>
+        <Link/>
     </div>
   )
 }
 
 export default App
+
+
+
---
+++ src/components/Link.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Link = () => {
+    return (
+        <button  className='button-86 bouncy'  ><a href="https://personalblogs.vercel.app/" target="_blank" rel="noreferrer">Portfolio</a></button>
+    )
+}
+
+export default Link
